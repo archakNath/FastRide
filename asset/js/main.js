ca9e09ef4f711aa1b8e24e0d1bcec08b05dbe52a
Archak Nath <112867859+archakNath@users.noreply.github.com>
2024-10-17 08:07:02 +0530

    add message box and geolocation

---
+++ asset/js/main.js
@@ -0,0 +1,35 @@
+const locateButton = document.querySelector(".locateButton");
+
+locateButton.onclick = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          function(position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            locationInput.value = `Latitude: ${latitude}, Longitude: ${longitude}`;
+          },
+          function(error) {
+            console.error("Error getting geolocation: ", error.message);
+          }
+        );
+      } else {
+        console.error("Geolocation is not supported by this browser.");
+      }
+      
+}
+
+const findRouteButton = document.querySelector("main .text-content button");
+const locationInput = document.querySelector(".locationInput");
+const destinationInput = document.querySelector(".destinationInput");
+const dialogBox = document.querySelector(".message");
+const dialogCancelButton = document.querySelector(".message .message-box button");
+
+findRouteButton.onclick = () => {
+    if(locationInput.value == '' || destinationInput.value == ''){
+        dialogBox.style.display = "flex";
+    }
+}
+
+dialogCancelButton.onclick = () => {
+    dialogBox.style.display = "none";
+}
